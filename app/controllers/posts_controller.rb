b82abe6337b7b7c784a87c5d5b30b5bcf3370418
Umar Ghouse <m.umarghouse@gmail.com>
2020-03-08 12:59:54 +0530

    Added image captions for hero image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -78,6 +78,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :hero_image)
+      params.require(:post).permit(:title, :body, :hero_image, :caption)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_130543) do
+ActiveRecord::Schema.define(version: 2020_03_08_071924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_03_07_130543) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "caption"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
